f5eedd8d5ba427641e8831bfa3ee89213c89cb74
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-24 12:43:54 -0500

    Sending Emails with SendGrid

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,7 +1,8 @@
 const nodeMailer = require('nodemailer');
 require('dotenv').config();
+const sgMail = require('@sendgrid/mail');
 
-exports.sendEmail = async(req, res) => {
+exports.sendEmailMailtrap = async(req, res) => {
     const testAccount = await nodeMailer.createTestAccount();
 
     const transporter = nodeMailer.createTransport({
@@ -17,7 +18,20 @@ exports.sendEmail = async(req, res) => {
         from: "Codie",
         to: "user@example.com",
         subject: "Example",
-        html:' <h2>Sending emails with Node.js</h2>'
+        html:' <h2>Sending esmails with Node.js</h2>'
     })
     res.json(info);
+};
+
+exports.sendEmail = async (req, res) => {
+    sgMail.setApiKey(process.env.MAIL_API_KEY);
+    const msg = {
+        to: 'logangroth@gmail.com', // Change to your recipient
+        from: 'codiestover.cs@gmail.com', // Change to your verified sender
+        subject: 'Test Email',
+        text: 'You have been selected to receive my test email!',
+        html: '<strong>sorry dude</strong>',
+    };
+        const info = await sgMail.send(msg);
+        res.json(info);
 };
