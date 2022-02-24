aad5e9edf168bbd443cb21609d79ddd1ab7314b3
Codie <77300264+codiemaureen@users.noreply.github.com>
2022-02-24 12:12:51 -0500

    Sending Emails

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -1,3 +1,23 @@
+const nodeMailer = require('nodemailer');
+require('dotenv').config();
+
 exports.sendEmail = async(req, res) => {
-    res.send('Sending Email');
+    const testAccount = await nodeMailer.createTestAccount();
+
+    const transporter = nodeMailer.createTransport({
+        host: process.env.MAIL_HOST,
+        port: process.env.MAIL_PORT,
+        auth: {
+            user: process.env.MAIL_USER,
+            pass: process.env.MAIL_PASS
+        }
+    });
+
+    const info = await transporter.sendMail({
+        from: "Codie",
+        to: "user@example.com",
+        subject: "Example",
+        html:' <h2>Sending emails with Node.js</h2>'
+    })
+    res.json(info);
 };
